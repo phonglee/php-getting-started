ec252faddeb36feaea1880795883ec7d9bd8a412
Phong Le <phongle@kms-technology.com>
2016-11-07 14:09:55 +0700

    use index.php

--- web/views/sendemail.php
+++ web/views/sendemail.php
@@ -14,23 +14,23 @@ if ($mail) {
 	echo "Message delivery failed..."
 }
 
-$name       = @trim(stripslashes($_POST['name'])); 
-$from       = @trim(stripslashes($_POST['email']));
-$phone 		= @trim(stripslashes($_POST['phone']));
-$subject    = @trim(stripslashes($_POST['subject'])); 
-$message    = @trim(stripslashes($_POST['message'])); 
-$to   		= 'phongle2512@gmail.com';//replace with your email
+// $name       = @trim(stripslashes($_POST['name'])); 
+// $from       = @trim(stripslashes($_POST['email']));
+// $phone 		= @trim(stripslashes($_POST['phone']));
+// $subject    = @trim(stripslashes($_POST['subject'])); 
+// $message    = @trim(stripslashes($_POST['message'])); 
+// $to   		= 'phongle2512@gmail.com';//replace with your email
 
-$headers   = array();
-$headers[] = "MIME-Version: 1.0";
-$headers[] = "Content-type: text/plain; charset=iso-8859-1";
-$headers[] = "From: {$name}-{$phone} <{$from}>";
-$headers[] = "Reply-To: <{$from}>";
-$headers[] = "Subject: {$subject}";
-$headers[] = "X-Mailer: PHP/".phpversion();
+// $headers   = array();
+// $headers[] = "MIME-Version: 1.0";
+// $headers[] = "Content-type: text/plain; charset=iso-8859-1";
+// $headers[] = "From: {$name}-{$phone} <{$from}>";
+// $headers[] = "Reply-To: <{$from}>";
+// $headers[] = "Subject: {$subject}";
+// $headers[] = "X-Mailer: PHP/".phpversion();
 
-#mail($to, $subject, $message, $headers);
-#echo "Thank you for contacting us!";
+//mail($to, $subject, $message, $headers);
+//echo "Thank you for contacting us!";
 
 die;
 ?>
