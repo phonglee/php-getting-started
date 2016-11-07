0c39a6eee9a63608565be44f649b4e0a9ed5864b
Phong Le <phongle@kms-technology.com>
2016-11-07 13:37:40 +0700

    Test email

--- web/views/sendemail.php
+++ web/views/sendemail.php
@@ -14,8 +14,21 @@ $headers[] = "Reply-To: <{$from}>";
 $headers[] = "Subject: {$subject}";
 $headers[] = "X-Mailer: PHP/".phpversion();
 
-mail($to, $subject, $message, $headers);
-echo "Thank you for contacting us!";
+#mail($to, $subject, $message, $headers);
+#echo "Thank you for contacting us!";
+$mail = mail('phongle2512@gmail.com', 'My Subject', 'Order...');
+if ($mail) {
+	echo "Message sent!"
+} else {
+	echo "Message delivery failed..."
+}
+$mail = mail('hai_phong2512@yahoo.com', 'My Subject', 'Order...');
+if ($mail) {
+	echo "Message sent!"
+} else {
+	echo "Message delivery failed..."
+}
+
 
 die;
 ?>
